1ec251581af2077910cf900808a713b07ff5ecd6
João Faria <joao.faria@astro.up.pt>
2016-02-16 17:05:23 +0000

    add barebones for problem sheet 1

---
+++ practical/sheet1_ex2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+# number of iterations to run MCMC
+
+
+# set p as the posterior distribution (the distribution we want to sample from)
+
+
+# initial condition
+x = []
+
+
+# iterate
+for step in :
+	# propose a step
+	d = np.random.
+
+	# calculate the ratio of posterior probabilities
+	ratio =
+
+	if ratio > 1.:
+		# accept the new step
+	else:
+		# always reject!
---
+++ practical/sheet1_ex3.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+# number of iterations to run MCMC
+
+
+# set p as the posterior distribution (the distribution we want to sample from)
+
+
+# initial condition
+x = []
+
+
+# number of accepted steps
+n_accept = 0
+
+
+# iterate
+for step in :
+	# propose a step
+	d = np.random.
+
+	# calculate the ratio of posterior probabilities
+	ratio =
+
+	if ratio > 1.:
+		# accept the new step
+		# increase n_accept
+	else:
+		# accept with probability = ratio
+
+
+# acceptance ratio
+acceptance = 
