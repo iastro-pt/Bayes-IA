ab263a5e3b8b0a05344eb56968b8137df6ba72ff
João Faria <joao.faria@astro.up.pt>
2016-02-17 18:21:47 +0000

    add solutions to problem sheet 1

--- practical/sheet1_ex3.py
+++ practical/sheet1_ex3.py
@@ -3,33 +3,60 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 # number of iterations to run MCMC
-
+niter = 1000
 
 # set p as the posterior distribution (the distribution we want to sample from)
-
+def p(x):
+	return norm(loc=0, scale=1.).pdf(x)
 
 # initial condition
-x = []
+x = [0.1]
 
 
 # number of accepted steps
 n_accept = 0
 
-
 # iterate
-for step in :
+for step in range(niter):
 	# propose a step
-	d = np.random.
+	### change here the scale of the proposal distribution ###
+	scale = 0.1  # or 10
+	d = scale*np.random.randn()
+
+	# candidate
+	xnew = x[-1] + d
 
 	# calculate the ratio of posterior probabilities
-	ratio =
+	ratio = p(xnew) / p(x[-1])
 
 	if ratio > 1.:
 		# accept the new step
-		# increase n_accept
+		x.append(xnew)
+		n_accept += 1
 	else:
 		# accept with probability = ratio
+		u = np.random.uniform()
+		if ratio > u:
+			x.append(xnew) # accept the new step
+			n_accept += 1
+		else:
+			x.append(x[-1])
+ 
+
+print 'Acceptance fraction', n_accept/float(niter)
 
 
-# acceptance ratio
-acceptance = 
+t = np.linspace(-6, 6, 1000)
+plt.figure()
+plt.subplot(211)
+plt.plot(t, p(t), 'k-', lw=3, label='target distribution')
+plt.hist(x, bins=50, normed=True, alpha=0.7, label='samples')
+plt.axvline(np.mean(x), lw=4, ls='--', color='r', label='mean of samples')
+plt.xlim([-5, 5])
+# plt.legend()
+plt.subplot(212)
+plt.plot(x)
+plt.xlabel('iteration')
+plt.ylabel(r'sampled $x$')
+plt.xlim([-0.05*niter, 1.05*niter])
+plt.show()
--- practical/sheet1_ex4.py
+++ practical/sheet1_ex4.py
@@ -1,36 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import ...
+from scipy.stats import norm, uniform, beta
 
 # number of iterations to run MCMC
-
+niter = 1000
 
 # set p as the posterior distribution (the distribution we want to sample from)
-# use distributions from Scipy
+def p(x):
+	return beta(a=0.5, b=0.5).pdf(x)
 
 
 # initial condition
-x = []
+x = [0.1]
 
 
 # number of accepted steps
 n_accept = 0
 
-
 # iterate
-for step in :
+for step in range(niter):
 	# propose a step
-	d = np.random.
+	d = 1*np.random.randn()
+
+	# candidate
+	xnew = x[-1] + d
 
 	# calculate the ratio of posterior probabilities
-	ratio =
+	ratio = p(xnew) / p(x[-1])
 
 	if ratio > 1.:
 		# accept the new step
-		# increase n_accept
+		x.append(xnew)
+		n_accept += 1
 	else:
 		# accept with probability = ratio
+		u = np.random.uniform()
+		if ratio > u:
+			x.append(xnew) # accept the new step
+			n_accept += 1
+		else:
+			x.append(x[-1])
+ 
+
+print 'Acceptance fraction', n_accept/float(niter)
 
 
-# acceptance ratio
-acceptance = 
+t = np.linspace(-6, 6, 1000)
+plt.figure()
+plt.subplot(211)
+plt.plot(t, p(t), 'k-', lw=3, label='target distribution')
+plt.hist(x, bins=50, normed=True, alpha=0.7, label='samples')
+plt.axvline(np.mean(x), lw=4, ls='--', color='r', label='mean of samples')
+plt.xlim([-5, 5])
+# plt.legend()
+plt.subplot(212)
+plt.plot(x)
+plt.xlabel('iteration')
+plt.ylabel(r'sampled $x$')
+plt.xlim([-0.05*niter, 1.05*niter])
+plt.show()
